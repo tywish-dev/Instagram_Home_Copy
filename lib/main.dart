6dec4aba6ab408ce129eba0e10ea51d4ebbd1da8
Samet Yılmaz <55537987+tywish-dev@users.noreply.github.com>
2023-03-04 15:35:02 +0300

    added cloud firestore package

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:instagram_home_copy/view/screens/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
