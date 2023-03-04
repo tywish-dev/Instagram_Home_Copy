16c2c130cff0e4d595d17b271a8ae1c778b0b39d
Samet Yılmaz <55537987+tywish-dev@users.noreply.github.com>
2023-03-04 14:06:20 +0300

    edited main.dart to init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_home_copy/view/screens/home_screen.dart';
 import 'package:instagram_home_copy/view/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
